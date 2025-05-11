106b955b7cf250985e6766c83fd24a834591f3dc
colaola20 <soryo@farmingdale.edu>
2025-05-11 01:36:29 -0400

    implemented game-filter

--- index.js
+++ index.js
@@ -110,10 +110,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let unfoundedgames = GAMES_JSON.filter((game) => {
+        return game['goal'] > game['pledged'];
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfoundedgames);
 }
 
 // show only games that are fully funded
@@ -121,10 +123,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let findedgames = GAMES_JSON.filter((game) => {
+        return game['goal'] <= game['pledged'];
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(findedgames);
 }
 
 // show all games
@@ -132,7 +136,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -141,7 +145,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+document.getElementById("unfunded-btn").addEventListener("click", filterUnfundedOnly);
+document.getElementById("funded-btn").addEventListener("click", filterFundedOnly);
+document.getElementById("all-btn").addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
